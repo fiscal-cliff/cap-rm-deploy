54cf25daf1fd782a40fe21c72f185cffdd9457da
fox <fox@LM-PC.local>
2015-04-09 09:48:19 +0300

    new version

--- lib/cap/rm/deploy/version.rb
+++
@@ -1,7 +0,0 @@
-module Cap
-  module Rm
-    module Deploy
-      VERSION = "0.1.0"
-    end
-  end
-end
--- spec/cap/rm/deploy_spec.rb
+++ spec/cap/rm/deploy_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
-describe Cap::Rm::Deploy do
+describe Capistrano::Rmdeploy do
   it 'has a version number' do
-    expect(Cap::Rm::Deploy::VERSION).not_to be nil
+    expect(Capistrano::Rmdeploy::VERSION).not_to be nil
+  end
+
+  describe "#configure" do
+    before(:each) do
+      Capistrano::Rmdeploy.configure do |config|
+        config.user = 'user3'
+        config.site = 'http://localhost:3000'
+        config.password = 'admin'
+        config.status_id_to_update = [1,3]
+        config.done_status_id = 3
+        config.key = 'qSHC6HhOiAAQVECJv4Ig'
+      end
+    end
+
+    it "ensures that user is admin" do
+      i = Capistrano::Rmdeploy::Issue.new
+      expect(i.singleton_class.send(:user)).to eq('user3')
+    end
+
+    it "ensures that site is http://localhost:3000" do
+      i = Capistrano::Rmdeploy::Issue.new
+      expect(i.singleton_class.send(:site)).to eq(URI('http://localhost:3000'))
+    end
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
